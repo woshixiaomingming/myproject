9c53d385ad305e10aff1b365cf36f8da2d093c16
woshixiaomingming <986370232@qq.com>
2018-04-10 11:25:51 +0800

    增加反射的方式

--- src/main/java/com/util/Data.java
+++ src/main/java/com/util/Data.java
@@ -17,6 +17,18 @@ public class Data {
         return QueryT(sql, param, db, classEntity);
     }
 
+    public static int insert (String sql, Object[] param) {
+        return insert(sql, param, db);
+    }
+
+    public static int update (String sql, Object[] param) {
+        return update(sql, param, db);
+    }
+
+    public static boolean doTrans (List<Allsql> sql) {
+        return doTrans(sql, db);
+    }
+
     public static <T extends Bean> List<T> QueryT (String sql, Object[] param, String db, Class<T> classEntity) {
         Connection connection = null;
         PreparedStatement statement = null;
@@ -49,7 +61,7 @@ public class Data {
      * @param <T>
      * @return
      */
-    public static <T extends Bean> int update (String sql, Object[] param, String db, Class<T> classEntity) {
+    public static int update (String sql, Object[] param, String db) {
         Connection connection = null;
         PreparedStatement statement = null;
         int result = -1;
@@ -67,8 +79,69 @@ public class Data {
         return result;
     }
 
-    public static <T extends Bean> int updateTrans (List<> sql, Object[] param, String db, Class<T> classEntity) {
-
+    public static boolean doTrans (List<Allsql> sql, String db) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = com.util.Connection.getConnection(db);
+            connection.setAutoCommit(false);
+            for (Allsql sqls : sql) {
+                 statement = connection.prepareStatement(sqls.getSql());
+                 SQLParamHelper.setParam(sqls.getObjects(), statement);
+                 try {
+                     statement.executeUpdate();
+                 } catch (Exception e) {
+                     e.printStackTrace();
+                     logger.error("更新数据事务控制异常");
+                     connection.rollback();
+                     connection.setAutoCommit(true);
+                     return false;
+                 }
+            }
+            connection.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("使用当前数据源" + db + "的连接异常");
+            return false;
+        } finally {
+            com.util.Connection.closeConnection(connection, null, statement);
+        }
+        return true;
     }
 
+    public static boolean delete (String sql, Object[] param, String db) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = com.util.Connection.getConnection(db);
+            statement = connection.prepareStatement(sql);
+            SQLParamHelper.setParam(param, statement);
+            return statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("使用当前数据源" + db + "的连接异常");
+            return false;
+        } finally {
+            com.util.Connection.closeConnection(connection, null, statement);
+        }
+    }
+
+    public static int insert (String sql, Object[] param, String db) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        int result = -1;
+        try {
+            connection = com.util.Connection.getConnection(db);
+            statement = connection.prepareStatement(sql);
+            SQLParamHelper.setParam(param, statement);
+            result = statement.executeUpdate();
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("使用当前数据源" + db + "的连接异常");
+            return result;
+        } finally {
+            com.util.Connection.closeConnection(connection, null, statement);
+        }
+    }
 }
