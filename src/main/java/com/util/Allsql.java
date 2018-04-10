c947197d0f9025f475d502e232ecd4c285dca116
woshixiaomingming <986370232@qq.com>
2018-04-10 11:25:29 +0800

    增加反射的方式

---
+++ src/main/java/com/util/Allsql.java
@@ -0,0 +1,24 @@
+package com.util;
+
+public class Allsql {
+
+    private String sql;
+
+    private Object[] objects;
+
+    public String getSql() {
+        return sql;
+    }
+
+    public void setSql(String sql) {
+        this.sql = sql;
+    }
+
+    public Object[] getObjects() {
+        return objects;
+    }
+
+    public void setObjects(Object[] objects) {
+        this.objects = objects;
+    }
+}
